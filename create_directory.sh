c7fd54a214efcf0d039ac3499acf6fb3c2398cab
Ismael <ismaelsalifou03@gmail.com>
2025-05-09 18:14:59 +0200

    Add files via upload

--- create_directory.sh
+++ create_directory.sh
@@ -1,2 +1,8 @@
-#!bin?bash
-mkdir test_directory
+#!/bin/bash
+if [ ! -d "test_directory" ]; then
+    mkdir test_directory
+    echo "Le repertoire  a été créé"
+else
+    echo "Le repertoire existe"
+fi
+#ca virifie si le dossier n'existe pas et ca le cree
