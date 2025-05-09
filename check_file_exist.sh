13b5477372aa136073db94a816c95cfec0118bf5
Ismael <ismaelsalifou03@gmail.com>
2025-05-09 18:38:06 +0200

    Add files via upload

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-touch sample.txt
-fichier="sample.txt"
-if [ -f "$fichier" ]; then
-    echo "Le fichier '$fichier'existe"
+#!/bin/bash
+#ca demande le nom du fichier
+echo "Entrez le nom du fichier :"
+read sample
+#Vérifie si le fichier existe
+if [ -f "$sample.txt" ]; then
+    echo "Le fichier '$sample.txt' existe."
 else
-    echo "Le fichier '$fichier' existe pas"
+    echo "Le fichier '$sample.txt'existe pas."
 fi
 
 #ca nous dit si le fichier sample existe ou non
